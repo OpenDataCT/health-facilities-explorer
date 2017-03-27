fb41c75297f69af950d3d849aa26a99a4a0fa06f
Tyler Kleykamp <OpenDataCT@users.noreply.github.com>
2017-03-27 10:18:30 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1U7U5tFFUsyf5fW2pKtxlU_8_Brq8_B-V5aBzC9PpdAI/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1U7U5tFFUsyf5fW2pKtxlU_8_Brq8_B-V5aBzC9PpdAI/edit#gid=164271551';
